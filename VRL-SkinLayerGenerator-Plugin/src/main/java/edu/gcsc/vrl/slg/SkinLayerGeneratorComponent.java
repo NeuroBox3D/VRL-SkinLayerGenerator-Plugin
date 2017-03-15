f4fbc58ebdd11695d82658cfe6e8b410ada7688f
stephan <stephan@syntaktischer-zucker.de>
2017-03-15 13:54:56 -0400

    First working version.

--- VRL-SkinLayerGenerator-Plugin/src/main/java/edu/gcsc/vrl/slg/SkinLayerGeneratorComponent.java
+++ VRL-SkinLayerGenerator-Plugin/src/main/java/edu/gcsc/vrl/slg/SkinLayerGeneratorComponent.java
@@ -3,6 +3,7 @@ package edu.gcsc.vrl.slg;
 import edu.gcsc.vrl.ug.api.I_SkinLayerGenerator;
 import edu.gcsc.vrl.ug.api.SkinLayerGenerator;
 import eu.mihosoft.vrl.annotation.ComponentInfo;
+import eu.mihosoft.vrl.annotation.ParamInfo;
 import java.io.Serializable;
 
 /**
@@ -16,16 +17,51 @@ public class SkinLayerGeneratorComponent implements Serializable {
 
     /**
      * @brief add layer
+     * @param name
+     * @param height
+     * @param res
      */
-    public void add_layer() {
-	    
+    public void add_layer(
+	    @ParamInfo(name="Name") 
+	    String name,
+	    @ParamInfo(name="Height")
+	    Double height,
+	    @ParamInfo(name="Resolution")
+	    Double res
+    ) {
+	    slg.add_layer(height, res, name);
     }
 
     /**
      * @brief add layer with depot
+     * @param name
+     * @param height
+     * @param res
+     * @param depotName
+     * @param depotPos
+     * @param depotRes
+     * @param depotWidth
+     * @param depotHeight
      */
-    public void add_layer_with_depot() {
-	    
+    public void add_layer_with_depot(
+	    @ParamInfo(name="Name") 
+	    String name,
+	    @ParamInfo(name="Height")
+	    Double height,
+	    @ParamInfo(name="Resolution")
+	    Double res,
+	    @ParamInfo(name="Depot's name")
+	    String depotName,
+	    @ParamInfo(name="Depot's position")
+	    Double depotPos,
+	    @ParamInfo(name="Depot's resolution")
+	    Double depotRes,
+	    @ParamInfo(name="Depot's width")
+	    Double depotWidth,
+	    @ParamInfo(name="Depot's height")
+	    Double depotHeight
+    ) {
+	slg.add_layer_with_injection(height, res, name, depotName, depotHeight, depotWidth, depotPos);
     }
     
     /**
